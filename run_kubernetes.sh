6e083b33ccb15f6074217a910a6c9cc29efa7305
Daphne-Charles <daphneacharles@gmail.com>
2020-09-04 18:55:45 -0400

    finished files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,13 +9,15 @@ dockerpath=daphneacharles/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run api --image=$dockerpath --port=80
-
-
+#kubectl run project4 --image=$dockerpath --port=80 --image-pull-policy=Never
+#docker login --username daphneacharles
+#kubectl run project4 --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app=project4
+kubectl run project4 --generator=run-pod/v1 --image=$dockerpath --port=80
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/project4 8000:80
+#kubectl port-forward deployment/project4 8000:80
+kubectl port-forward pod/project4 8000:80
